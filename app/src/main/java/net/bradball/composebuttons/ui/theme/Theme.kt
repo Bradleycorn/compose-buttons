46ed14c654a7f1e859afa8dd6f82f6c05f08e34b
Brad Ball <brad.ball@twinspires.com>
2022-01-03 11:19:32 -0500

    Re-add dark theme support

--- app/src/main/java/net/bradball/composebuttons/ui/theme/Theme.kt
+++ app/src/main/java/net/bradball/composebuttons/ui/theme/Theme.kt
@@ -29,7 +29,7 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun ComposeButtonsTheme(
-    darkTheme: Boolean = false, //isSystemInDarkTheme(),
+    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
     val colors = if (darkTheme) {
